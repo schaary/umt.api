a814bf93816e8cb094161828718b604bbf86d82f
Michael Schaarschmidt <michael.schaarschmidt@itz.uni-halle.de>
2013-02-13 00:07:40 +0100

    remove deprecated commands from person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,8 @@
 class Person < ActiveRecord::Base
   # specify schema and table name
-  set_table_name "people"
+  self.set_table_name "people"
   # specify primary key name
-  set_primary_key "username"
+  self.set_primary_key "username"
 
   attr_accessible :username, :firstname, :lastname, :birthday_on, :gender
   # attr_accessible :title, :body
