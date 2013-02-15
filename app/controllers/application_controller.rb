b29a42ae07ebcb784903ab92e06a0c3c499fb8d2
Michael Schaarschmidt <michael.schaarschmidt@itz.uni-halle.de>
2013-02-15 18:25:02 +0100

    clean up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,7 @@ class ApplicationController < ActionController::API
 
   def restrict_access
     authenticate_or_request_with_http_token do |token, options|
-      ApiToken.exists? token: 'dc06aee48634f6bdc93455fa33038c6a90f58e182f2bbf8dd7fbff9d330b8508cdf0a2658adba939'
+      ApiToken.exists? token: token 
     end
   end
-# dc06aee48634f6bdc93455fa33038c6a90f58e182f2bbf8dd7fbff9d330b8508cdf0a2658adba939
 end
